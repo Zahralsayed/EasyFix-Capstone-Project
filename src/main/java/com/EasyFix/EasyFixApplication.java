a6f4966872f0b9e316efa90eb8bd758c457e7cc0
Zahralsayed <zalmatooq31@gmail.com>
2026-05-06 19:08:16 +0300

    initializer

---
+++ src/main/java/com/EasyFix/EasyFixApplication.java
@@ -0,0 +1,13 @@
+package com.EasyFix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyFixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EasyFix/EasyFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EasyFix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
