0f0bf171515f16833de4e486577f87e1ceb2ab71
Zahralsayed <zalmatooq31@gmail.com>
2026-05-13 01:02:15 +0300

    User model

---
+++ src/main/java/com/EasyFix/model/User.java
@@ -0,0 +1,50 @@
+package com.EasyFix.model;
+
+
+import com.EasyFix.enums.Role;
+import com.EasyFix.enums.UserStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String verificationToken;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private String phoneNumber;
+    private String address;
+    private String profilePicture;
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+    private boolean isVerified = false;
+    private String resetToken;
+
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDate createdAt;
+    @UpdateTimestamp
+    private LocalDate updatedAt;
+}
