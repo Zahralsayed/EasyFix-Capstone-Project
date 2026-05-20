c5db0acdf45aa1fdceba390d8b1b2e90ac932767
Zahralsayed <zalmatooq31@gmail.com>
2026-05-21 02:42:48 +0300

    user Repo updates

--- src/main/java/com/EasyFix/repository/UserRepository.java
+++ src/main/java/com/EasyFix/repository/UserRepository.java
@@ -3,5 +3,12 @@ package com.EasyFix.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.EasyFix.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationToken(String token);
+    Optional<User> findByResetToken(String resetToken);
+
 }
