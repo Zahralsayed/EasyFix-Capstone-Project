628f5286b205e3c3f8cfa25263caa6e626acffbb
Zahralsayed <zalmatooq31@gmail.com>
2026-05-24 03:19:01 +0300

    Portfolio Response DTO

---
+++ src/main/java/com/EasyFix/dto/PortfolioResponseDTO.java
@@ -0,0 +1,10 @@
+package com.EasyFix.dto;
+
+import lombok.Data;
+
+@Data
+public class PortfolioResponseDTO {
+    private Long id;
+    private String workImage;
+    private String description;
+}
